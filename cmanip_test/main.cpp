b9075dae548dd638f09eecd95fb78b971fef1614
REK7on <tansang.27.10.2003@gmail.com>
2021-11-01 10:02:45 +0700

    remove using namespace std; for clarity

--- cmanip_test/main.cpp
+++ cmanip_test/main.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
+
 #include "cmanip.h"
-using namespace std;
 
 int main() {
   setConsoleColor(LIGHT_GREEN, LIGHT_BLUE);
   setWindowSize(30, 20);
   setConsoleCursorPosition(10, 20);
   setConsoleTextColor(YELLOW, RED);
-  cout << "Test";
+  std::cout << "Test";
   return 0;
 }
